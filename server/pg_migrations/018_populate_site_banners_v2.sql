b55e4a287650b37f23d66588695c59d3986ceff5
Jon Pamment <jon@skyhigh.com.au>
2026-05-18 08:53:12 +1000

    [FIX] Migration 018: Properly populate site banners with random working images
    Fixes bugs in migration 014:
    - Cartesian product bug where library was joined with itself
    - Empty banner strings were treated as valid options
    - Now correctly filters non-empty banners only
    
    This will populate all 74 sites with random banner URLs from the working
    R2 bucket (pub-d31362da23d54f83bb50efb9194c6b87.r2.dev) based on site type.

---
+++ server/pg_migrations/018_populate_site_banners_v2.sql
@@ -0,0 +1,36 @@
+-- Populate site banners with random images from library (v2)
+-- Fixes bugs in migration 014:
+-- 1. Cartesian product bug in CTE (using both library_data and jsonb_array_elements as table reference)
+-- 2. Empty banner strings were being included as valid options
+-- 3. Uses correct working R2 bucket URLs
+
+WITH library_data AS (
+  SELECT value::jsonb AS lib
+  FROM settings
+  WHERE key = 'imageLibrary'
+),
+all_images AS (
+  SELECT
+    elem ->> 'banner' AS banner,
+    elem ->> 'category' AS category
+  FROM library_data,
+  jsonb_array_elements(lib) AS elem
+  WHERE (elem ->> 'banner') IS NOT NULL
+    AND (elem ->> 'banner') != ''
+),
+coastal_images AS (
+  SELECT banner FROM all_images
+  WHERE category IS NULL OR category = 'coastal'
+),
+inland_images AS (
+  SELECT banner FROM all_images
+  WHERE category IS NULL OR category = 'inland'
+)
+UPDATE sites
+SET image = CASE
+  WHEN lower(coalesce(type, '')) ~ 'inland|mountain|ridge|tow' THEN
+    (SELECT banner FROM inland_images ORDER BY random() LIMIT 1)
+  ELSE
+    (SELECT banner FROM coastal_images ORDER BY random() LIMIT 1)
+END
+WHERE image IS NULL OR image = '';
